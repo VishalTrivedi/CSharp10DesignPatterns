4c96e664c6285479d63a84cb86647aba76300209
Vishal Trivedi <VishalTrivedi@users.noreply.github.com>
2022-07-03 21:27:14 -0700

    Finished Singleton

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,45 @@
 ﻿namespace Singleton
 {
-  
+	/// <summary>
+	/// Singleton
+	/// </summary>
+	public class Logger
+	{
+		protected Logger()
+		{
+
+		}
+
+		// Lazy<T>
+		private static readonly Lazy<Logger> _lazylogger
+			= new Lazy<Logger>(() => new Logger());
+
+		//private static Logger? _instance;
+
+		
+		/// <summary>
+		/// Instance
+		/// </summary>
+		public static Logger Instance
+		{
+			get 
+			{
+				return _lazylogger.Value; 
+				//if(_instance == null)
+				//{
+				//	_instance = new Logger();
+				//}
+				//return _instance;
+			}
+		}
+
+		/// <summary>
+		/// SingletonOperation
+		/// </summary>
+		/// <param name="message"></param>
+		public void Log(string message)
+		{
+			Console.WriteLine($"Message to log: {message}");
+		}
+	}  
 }
--- Starter solution/Singleton/Program.cs
+++ Starter solution/Singleton/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+//Console.WriteLine("when can i do Minecratft");
+
+// Call the property getter twice
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2
+		&& instance2 == Logger.Instance)
+{
+	Console.WriteLine("Instances are the same.");
+}
+
+instance1.Log($"Message from {nameof(instance1)}");
+// or
+instance1.Log($"Message from {nameof(instance2)}");
+// or
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
