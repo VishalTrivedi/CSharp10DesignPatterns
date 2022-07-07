bc96f3b0c191155e28c18d96108f1b42156f5ce9
Vishal Trivedi <VishalTrivedi@users.noreply.github.com>
2022-07-06 21:17:41 -0700

    Finished Creational / Abstract Factory

--- Starter solution/AbstractFactory/Program.cs
+++ Starter solution/AbstractFactory/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.Title = "Abstract Factory";
+﻿using AbstractFactory;
+
+Console.Title = "Abstract Factory";
+
+Console.WriteLine("Belgium");
+var belgiumShoppingCartPurchaseFactory = new BelgiumShoppingCartPurchaseFactory();
+var shoppingCartForBelgium = new ShoppingCart(belgiumShoppingCartPurchaseFactory);
+shoppingCartForBelgium.CalculateCosts();
+
+Console.WriteLine("France");
+var franceShoppingCartPurchaseFactory = new FranceShippingCartPurchaseFactory();
+var shoppingCartForFrance = new ShoppingCart(franceShoppingCartPurchaseFactory);
+shoppingCartForFrance.CalculateCosts();
+
+Console.ReadKey();
