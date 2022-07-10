b6c55730653b88a2499731656bc1bcc9f15da7d7
Vishal Trivedi <VishalTrivedi@users.noreply.github.com>
2022-07-09 21:34:14 -0700

    Implemented Creational / Builder

--- Starter solution/Builder/Program.cs
+++ Starter solution/Builder/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder  = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(builder: miniBuilder);
+Console.WriteLine(miniBuilder.Car.ToString());
+// or:
+garage.Show();
+
+garage.Construct(builder: bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+// or:
+garage.Show();
+
+Console.ReadKey();
